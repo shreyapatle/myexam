17c8788565415f3bfc7635196e723a77f6ebc3c4
Shreya Patle <shreya.patle17@gmail.com>
2021-05-25 00:36:43 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./myComponent/header";
+import Banner from './myComponent/banner';
+import reactDom from 'react-dom';
+import React from 'react';
+import Widget from "./myComponent/widget";
+import Footer from "./myComponent/footer";
 
 function App() {
+  let widget=[
+    {
+      title: "Free to download",
+      desc: "As always, Start Bootstrap has a powerful collectin of free templates."
+    },
+    {
+      title: "Jumbotron hero header",
+      desc: "The heroic part of this template is the jumbotron hero header!" 
+    },
+    {
+      title: "Feature boxes",
+      desc: "We've created some custom feature boxes using Bootstrap icons!" 
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Banner />
+      <section className="pt-4">
+        <div className="container px-lg-5">
+          <div className="row gx-lg-5">
+           {widget.map(function(ele,index){
+              return <Widget title={ele.title} desc={ele.desc} key={index} />
+           })} 
+            {/* <Widget title={"Free to download"} desc={"As always, Start Bootstrap has a powerful collectin of free templates."}  />
+            <Widget title={"Jumbotron hero header"} desc={"The heroic part of this template is the jumbotron hero header!"} />
+            <Widget title={"Feature boxes"} desc={"We've created some custom feature boxes using Bootstrap icons!"} />
+            <Widget title={"Simple clean code"} desc={"We keep our dependencies up to date and squash bugs as they come!"} /> */}
+            <Footer/>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+
   );
 }
 
---
+++ src/myComponent/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <div>
+            <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        </div>
+    )
+}
